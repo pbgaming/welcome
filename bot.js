90575414cf79f08dbc9122181fc23f4a70f66323
pbgaming <44928010+pbgaming@users.noreply.github.com>
2018-12-31 00:00:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,18 +9,12 @@ setTimeout(() => mohamed.send(`**Welcome To Epic Shop**`), 4000)
 }
 });
 
-
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Cyhper Script By : DREAM`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Epic Shop`,"http://twitch.tv/YouTube")
-client.user.setStatus("dnd")
+client.on('guildMemberAdd', member => {
+const mohamed= member.guild.channels.get("520270925560807450");
+if(!mohamed) return;
+if(mohamed) {
+setTimeout(() => mohamed.send(`#role humans epic`), 4000)        
+}
 });
-
-
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
