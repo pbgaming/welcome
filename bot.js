5443f2d43c64b9d67bf2112cbdba6e93553c3e6e
pbgaming <44928010+pbgaming@users.noreply.github.com>
2019-01-28 19:20:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,7 +37,7 @@ client.on('guildMemberAdd', member => {
     invites[member.guild.id] = guildInvites;
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
-    const logChannel = member.guild.channels.find(channel => channel.name === "اسم روم الولكم");
+    const logChannel = member.guild.channels.find(channel => channel.name === "invite");
     logChannel.send(`${member} Invited by: <@${inviter.id}>`);
   });
 });
