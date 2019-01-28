36190491501f8f92d1d3ad02032d4c5fd61154e9
pbgaming <44928010+pbgaming@users.noreply.github.com>
2019-01-28 19:19:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,4 +17,29 @@ setTimeout(() => mohamed.send(`#role humans epic`), 4000)
 }
 });
 
+const invites = {};
+
+const wait = require('util').promisify(setTimeout);
+
+client.on('ready', () => {
+  wait(1000);
+
+  client.guilds.forEach(g => {
+    g.fetchInvites().then(guildInvites => {
+      invites[g.id] = guildInvites;
+    });
+  });
+});
+
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    invites[member.guild.id] = guildInvites;
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const logChannel = member.guild.channels.find(channel => channel.name === "اسم روم الولكم");
+    logChannel.send(`${member} Invited by: <@${inviter.id}>`);
+  });
+});
+
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
