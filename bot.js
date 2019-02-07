e3ce8a49f43a8263c040447b836b26ff0655f3c7
pbgaming <44928010+pbgaming@users.noreply.github.com>
2019-02-07 22:04:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -340,29 +340,4 @@ ${serverQueue.songs.map(song => `**${++index} -** ${song.title}`).join('\n')}
 	return undefined;
 });
 
-const adminprefix = "$";
-const devs = ['438379202031517697'];
-client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
-    
-if (message.content.startsWith(adminprefix + 'playing')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
-} else 
-  if (message.content.startsWith(adminprefix + 'setN')) {
-client.user.setUsername(argresult).then
-    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
-return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
-} else
-  if (message.content.startsWith(adminprefix + 'setA')) {
-client.user.setAvatar(argresult);
-  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
-      } else     
-if (message.content.startsWith(adminprefix + 'setT')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/idk");
-    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
-}
-});
-
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
